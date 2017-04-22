3573f29dcfee9de2047a727f9bc79e4175683538
Vladimir Nazarenko <nazarenko2010@gmail.com>
2017-04-23 01:23:28 +0300

    Adds docker

--- assembly/compiler_wrapper.py
+++ assembly/compiler_wrapper.py
@@ -1,9 +1,13 @@
 import enum
 import random
 import re
+import logging
 from subprocess import check_output, STDOUT, CalledProcessError
 from os import remove, makedirs, removedirs
 from os.path import isfile
+
+logger = logging.getLogger("compiler_wrapper")
+logger.setLevel(logging.DEBUG)
 
 
 class Compiler(enum.Enum):
@@ -14,7 +18,7 @@ class Compiler(enum.Enum):
 
 	def compiler_command(self):
 		if self.name.startswith("GCC"):
-			return "g++"
+			return "g++ -fdiagnostics-color=never"
 		elif self == Compiler.CLANG:
 			return "clang"
 		else:
@@ -52,7 +56,7 @@ def get_bash_command_for_compiler_execution(compiler, optimization_level, filena
 	return " ".join([
 		"docker run",
 		"-v `pwd`/{0}:/{0}".format(filename),
-		"-v `pwd`/output_dir:/out"
+		"-v `pwd`/{0}:/out".format(output_dir),
 		"--rm -it",
 		compiler.docker_image(),
 		"{0} /{1} -S -O{2} -o /out/{1}.s".format(compiler.compiler_command(), filename, optimization_level[-1])])
@@ -62,36 +66,45 @@ def compile_code(code, optimization_level, compiler_name):
 	# validate
 	if not validate_optimization_level(optimization_level) or not validate_code(code) or not validate_compiler(
 			compiler_name):
+		logger.critical("Couldn't validate the input")
 		return "Invalid input"
 
 	compiler = get_compiler_for_string(compiler_name)
 
+	logger.debug("Started compilation procedures")
+
 	# compile
 	file_id = str(random.randint(0, 1e9))
 	input_file_name = file_id + ".cpp"
+
+	logger.info("Writing the input into " + input_file_name)
+
 	with open(input_file_name, "w") as out:
 		out.write(code)
 	makedirs(file_id)
+
+	output_file_name = file_id + "/" + input_file_name + ".s"
+
 	try:
 		output = check_output(
 			get_bash_command_for_compiler_execution(compiler, optimization_level, input_file_name, file_id),
 							  stderr=STDOUT,
 							  shell=True)
 	except CalledProcessError as e:
-		output = "Compilation failed with following output:\n" + str(e.output)
+		output = "Compilation failed with following output:\n" + str(e.output.decode("ascii", "ignore"))
 	finally:
 		remove(input_file_name)
-
-	output_file_name = file_id + "/" + input_file_name + ".s"
 
 	if isfile(output_file_name):
 		with open(output_file_name) as assembly_file:
 			content = assembly_file.read()
 		remove(output_file_name)
-		removedirs(file_id)
+	elif output == "":
+		content = "Command was successful with no output"
+		logger.critical("Logical error in the compiler wrapper")
 	else:
-
 		content = output
 
+	removedirs(file_id)
 
 	return content
--- assembly/views.py
+++ assembly/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseForbidden
 from .compiler_wrapper import compile_code
+import logging
+
+logger = logging.getLogger("views")
 
 
 # Create your views here.
@@ -10,8 +13,11 @@ def index(request):
 
 def assembly(request):
 	if not request.is_ajax():
+		logger.critical("Not an ajax request")
 		return HttpResponseForbidden('<h1>No Page Here</h1>')
+	logger.info("Starting serving the request")
 	source = request.GET["code"]
+	compiler_name = request.GET["compiler"]
 	optimization_level = request.GET["optimization_level"]
-	compiled = compile_code(source, optimization_level)
+	compiled = compile_code(source, optimization_level, compiler_name)
 	return HttpResponse(compiled)
